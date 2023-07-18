489b4fe9acff7bf38947f6908b40b02c8ab67c8b
Reuben Brewer <reuben.brewer@gmail.com>
2023-07-18 11:23:03 -0700

    Add files via upload

--- test_program_for_LowPassFilter_ReubenPython2and3Class.py
+++ test_program_for_LowPassFilter_ReubenPython2and3Class.py
@@ -6,7 +6,7 @@ reuben.brewer@gmail.com
 www.reubotics.com
 
 Apache 2 License
-Software Revision H, 05/10/2023
+Software Revision I, 07/18/2023
 
 Verified working on: Python 2.7, 3.8 for Windows 8.1, 10 64-bit, Ubuntu 20.04, and Raspberry Pi Buster (no Mac testing yet).
 '''
